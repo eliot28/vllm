2b29adb4ac7cc0dc09b4124895c05163e403aac9
Prashant Gupta <prashantgupta@us.ibm.com>
2024-09-04 11:01:19 -0700

    🎨 format
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- vllm/prompt_adapter/utils.py
+++ vllm/prompt_adapter/utils.py
@@ -9,6 +9,7 @@ from safetensors.torch import load_file as safe_load_file
 WEIGHTS_NAME = "adapter_model.bin"
 SAFETENSORS_WEIGHTS_NAME = "adapter_model.safetensors"
 
+
 # Get current device name based on available devices
 def infer_device() -> str:
     if torch.cuda.is_available():
@@ -18,25 +19,25 @@ def infer_device() -> str:
     return "cpu"
 
 
-def load_peft_weights(
-    model_id: str, device: Optional[str] = None, **hf_hub_download_kwargs
-) -> dict:
+def load_peft_weights(model_id: str,
+                      device: Optional[str] = None,
+                      **hf_hub_download_kwargs) -> dict:
     r"""
     A helper method to load the PEFT weights from the HuggingFace Hub or locally
 
     Args:
         model_id (`str`):
-            The local path to the adapter weights or the name of the adapter to load from the HuggingFace Hub.
+            The local path to the adapter weights or the name of the adapter to
+            load from the HuggingFace Hub.
         device (`str`):
             The device to load the weights onto.
         hf_hub_download_kwargs (`dict`):
-            Additional arguments to pass to the `hf_hub_download` method when loading from the HuggingFace Hub.
+            Additional arguments to pass to the `hf_hub_download` method when 
+            loading from the HuggingFace Hub.
     """
-    path = (
-        os.path.join(model_id, hf_hub_download_kwargs["subfolder"])
-        if hf_hub_download_kwargs.get("subfolder", None) is not None
-        else model_id
-    )
+    path = (os.path.join(model_id, hf_hub_download_kwargs["subfolder"])
+            if hf_hub_download_kwargs.get("subfolder", None) is not None else
+            model_id)
 
     if device is None:
         device = infer_device()
@@ -52,11 +53,10 @@ def load_peft_weights(
         if token is None:
             token = hf_hub_download_kwargs.get("use_auth_token", None)
 
-        hub_filename = (
-            os.path.join(hf_hub_download_kwargs["subfolder"], SAFETENSORS_WEIGHTS_NAME)
-            if hf_hub_download_kwargs.get("subfolder", None) is not None
-            else SAFETENSORS_WEIGHTS_NAME
-        )
+        hub_filename = (os.path.join(hf_hub_download_kwargs["subfolder"],
+                                     SAFETENSORS_WEIGHTS_NAME)
+                        if hf_hub_download_kwargs.get("subfolder", None)
+                        is not None else SAFETENSORS_WEIGHTS_NAME)
         has_remote_safetensors_file = file_exists(
             repo_id=model_id,
             filename=hub_filename,
@@ -75,14 +75,14 @@ def load_peft_weights(
             )
         else:
             try:
-                filename = hf_hub_download(
-                    model_id, WEIGHTS_NAME, **hf_hub_download_kwargs
-                )
+                filename = hf_hub_download(model_id, WEIGHTS_NAME,
+                                           **hf_hub_download_kwargs)
             except EntryNotFoundError:
-                raise ValueError(
-                    f"Can't find weights for {model_id} in {model_id} or in the Hugging Face Hub. "
-                    f"Please check that the file {WEIGHTS_NAME} or {SAFETENSORS_WEIGHTS_NAME} is present at {model_id}."
-                )
+                raise ValueError(  # noqa: B904
+                    f"Can't find weights for {model_id} in {model_id} or \
+                    in the Hugging Face Hub. "
+                    f"Please check that the file {WEIGHTS_NAME} or \
+                    {SAFETENSORS_WEIGHTS_NAME} is present at {model_id}.")
 
     if use_safetensors:
         if hasattr(torch.backends, "mps") and (device == torch.device("mps")):
@@ -90,6 +90,7 @@ def load_peft_weights(
         else:
             adapters_weights = safe_load_file(filename, device=device)
     else:
-        adapters_weights = torch.load(filename, map_location=torch.device(device))
+        adapters_weights = torch.load(filename,
+                                      map_location=torch.device(device))
 
     return adapters_weights
